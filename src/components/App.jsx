7dd64e302eb5555487f212246252eb35c6e1bc43
RodionKipitok <kucernukrabota@gmail.com>
2023-10-04 15:41:15 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import css from './App.module.css'
+// import css from './App.module.css'
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 // import FriendsList from './FriendList/FriendsList';
 // import TransactionHistory from './TransactionHistory/TransactionHistory'
 import user from './Profile/user';
 import dataStatistics from './Statistics/data';
-import dataFriendsList from './FriendList/friends';
-import transactions from './TransactionHistory/transactions';
+// import dataFriendsList from './FriendList/friends';
+// import transactions from './TransactionHistory/transactions';
 
 
 
