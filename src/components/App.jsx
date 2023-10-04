0678c24228a9cdd11f47539b2801fc5b16bde79f
RodionKipitok <kucernukrabota@gmail.com>
2023-10-04 16:14:53 +0300

    s

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 // import css from './App.module.css'
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-// import FriendsList from './FriendList/FriendsList';
+import FriendsList from './FriendList/FriendsList';
 // import TransactionHistory from './TransactionHistory/TransactionHistory'
 import user from './Profile/user';
 import dataStatistics from './Statistics/data';
-// import dataFriendsList from './FriendList/friends';
+import dataFriendsList from './FriendList/friends';
 // import transactions from './TransactionHistory/transactions';
 
 
@@ -17,8 +17,8 @@ export const App = () => {
     <>
   <Profile {...user}/>
   <Statistics data = {dataStatistics}/>
-  {/* <FriendsList friends={dataFriendsList}/>
-  <TransactionHistory items={transactions} /> */}
+  <FriendsList friends={dataFriendsList}/>
+  {/* <TransactionHistory items={transactions} /> */}
   </>);
 
 
